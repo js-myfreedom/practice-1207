2c2e2a5b9752da8edd184c7aa7744432714ddb5b
Yury Tatsenka <forthepost@gmail.com>
2021-07-29 18:32:05 +0300

    add proptypes for Feedback component

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export function Feedback({name, feedback}) {
+export function Feedback({name = 222, feedback}) {
     return <>{name} :: {feedback}</>
+}
+
+Feedback.defaultProps = {
+    name: '123'
+}
+
+Feedback.propTypes = {
+    name: PropTypes.string,        
+    feedback: PropTypes.string.isRequired,
 }
